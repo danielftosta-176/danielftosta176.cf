f04c9ac88dee2337940da7c877e52c7f4c31ccc5
danielftosta-176 <danieltosta176@gmail.com>
2022-02-03 13:29:15 -0300

    Site is back

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var header_content = "Under Construction."
+
+window.onload = () => {
+	console.warn("🗈 main.js v:1.2.1")
+	console.warn("🗈 " + window.location.href)
+	document.getElementById("pre_header_m").innerHTML = header_content
+}
