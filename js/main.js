47f28cb68aee31ae1fa98f983e4963db3d22b3c7
danielftosta-176 <danielftosta@hotmail.com>
2022-03-07 18:36:46 -0300

    e

--- js/main.js
+++
@@ -1,7 +0,0 @@
-var header_content = "Under Construction."
-
-window.onload = () => {
-	console.warn("🗈 main.js v:1.2.1")
-	console.warn("🗈 " + window.location.href)
-	document.getElementById("pre_header_m").innerHTML = header_content
-}
