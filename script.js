75850bbac9353c1fe144bdcfa4279bb86e955103
danielftosta-176 <danielftosta@hotmail.com>
2022-03-07 23:10:36 -0300

    qw

--- script.js
+++ script.js
@@ -32,6 +32,85 @@ const navSlide = () => {
 	 hamburger.classList.toggle("toggle");
     }
 	 
+}
+
+const toEnglish = () => {
+	document.getElementById("gtranslate").onclick = () => {
+		document.body.innerHTML = `
+		  
+	<div class="container">
+	  
+	<!--────────────────Header───────────────-->
+<header>
+	<a class="logo" href="#home">
+		  <img src="0" alt=""" />
+	</a>
+	<nav>	
+	 <ul class="nav-bar"><div class="bg"></div>
+		<li><a class="nav-link active" href="#home">HOME</a></li>
+		<li><a class="nav-link" href="#pgs">PAGES</a></li>
+		<li><a class="nav-link" href="#rsc">SOCIAL MEDIA</a></li>
+	 </ul>
+		
+		<div class="hamburger">
+			<div class="line1"></div>
+		<div class="line2"></div>
+			<div class="line3"></div>
+		</div>
+	</nav>
+</header>
+<main>
+	<!--─────────────────Home────────────────-->
+  <div id="home">
+	 <div class="filter"></div>
+	 <section class="intro">
+	  <h3>DANIELFTOSTA176.<hr></h3>
+	  <p>New site.</p>	 
+	  <p id="gtranslate2">Portuguese version</p> 
+	  <!--────social media links─────-->
+		 
+	  <div class="social-media">
+		  <a href="https://www.youtube.com/channel/UCO1_19hDxivgnipU779R2OA" target="_blank"><i class="fab fa-youtube"></i></a>
+		</div>
+		 
+	 </section> 
+  </div>  
+	
+  <!--───────────────Projects───────────────-->
+  <div id="pgs"> 
+	 <h3>Pages.<hr></h3>
+	  <div class="work-box">
+	  <div class="work">
+	<!--───────────────card───────────────-->
+		<div class="card">
+			<img class="work-img" src="https://preview.redd.it/avjal33hpqo61.png?width=512&format=png&auto=webp&s=b652d83347448df74c8ba61c17b322e686ee32a4">
+			<a href="https://mcdl.DANIELFTOSTA176.cf/" target="_blank"> <!--Link to project-->
+			<div class="work-content">Minecraft Downloads</div></a>
+		</div>
+	  </div>
+	  </div>
+  </div>
+	 
+	<!--──────────────Contact────────────────-->
+  <div id="rsc">
+	  <!--────social media links─────-->
+	   <h3>Social media.<hr></h3>
+		<div class="social-media">
+		  <a href="https://www.youtube.com/channel/UCO1_19hDxivgnipU779R2OA" target="_blank"><i class='fab fa-youtube'></i></a>
+			<a onclick="alert('@DANIELFTOSTA')" target="_blank"><i class='fab fa-xbox'></i></a>
+		</div>
+
+	  </div>
+
+</main>
+  <footer class="copyright">© 2021 
+	  <a> DANIELFTOSTA176</a><a href="/another-page.html">.</a>
+ </footer>
+  
+</div>
+	`
 	}
+}
 
 window.onload = () => navSlide();
+window.onload = () => toEnglish();
