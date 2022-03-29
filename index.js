5c1d84c3c74d7febe293d916e38c74f555531e16
danielftosta-176 <danielftosta@hotmail.com>
2022-03-29 00:23:36 -0300

    d

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');//Set up the express module
+const app = express();
+const router = express.Router();
+const path = require('path')//Include the Path module
+
+//Set up the Express router
+router.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '/public_html/'));
+});
+app.use('/', router);
+
+//Navigate your website
+//if they go to '/lol'
+
+app.use('/lol', router);
+//404 Error
+app.use(function(req, res, next) {
+    res.status(404);
+    res.sendFile(__dirname + '/404.html');
+});
+
+
+//set up the Express server to listen on port 3000 and logs some messages when the server is ready
+let server = app.listen(3000, function(){
+  console.log("App server is running on port 3000");
+});
