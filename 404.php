7dc41d6f4e298a5421aa77af6eb221bec6811340
danielftosta-176 <danielftosta@hotmail.com>
2022-03-29 00:36:48 -0300

    e

---
+++ 404.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/assets/css/stylee.css">
+    <meta name="theme-color" content="#151515">
+    <meta name="msapplication-navbutton-color" content="#151515">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <title>DANIELFTOSTA176</title>
+    <meta name="description" content="Hi." />
+    <link rel="canonical" href="/another-page.html" />
+</head>
+<body>
+    <header>
+        <div class="container">
+            <a id="a-title" >
+                <h1>DANIELFTOSTA176.cf</h1>
+            </a>
+            <h2>404 Not Found</h2>
+        </div>
+    </header>
+    <div class="container">
+        <section id="main_content">
+            <p id="paaaragraf">404</p>
+					<a href="/TheHax">Voltar <<</a>
+        </section>
+    </div>
+</body>
+</html>
